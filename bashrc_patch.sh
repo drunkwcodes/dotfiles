3269e9116aba5d52591cd7cda4d4f810eb2ac39d
Drunkwcodes <drunkwcodes@gmail.com>
2019-03-10 11:51:38 +0000

    First commit

---
+++ bashrc_patch.sh
@@ -0,0 +1,18 @@
+
+# shortcuts
+
+alias cdp='cd ~/projects/'
+alias cddl='cd ~/Downloads/'
+
+# git aliases
+alias ga='git add'
+alias gb='git branch'
+alias gc='git commit -m'
+alias gco='git checkout'
+alias gd='git diff'
+alias glg='git lg'
+alias gp='git push'
+alias gpl='git pull --rebase'
+alias gs='git status'
+alias gsh='git stash'
+
