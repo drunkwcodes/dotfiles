4b99db30d5cbf1889dca91a8443c498382ece958
Drunkwcodes <drunkwcodes@gmail.com>
2019-04-23 22:49:42 +0000

    Update fedora_setup.sh to align with ubuntu_setup.sh

--- termux/fedora_setup.sh
+++ termux/fedora_setup.sh
@@ -1,15 +1,18 @@
 BACKUP=~/storage/shared/TermuxBackup
 if [ ! -d "$BACKUP" ]; then
     mkdir "$BACKUP"
+else
+    cp "$BACKUP"/* .  # ~/
 fi
 
-pkg install wget openssl-tool proot tar -y && hash -r && cp ~/storage/shared/TermuxBackup/* .
+pkg install wget openssl-tool proot tar -y && hash -r
 
 FILE=fedora-rootfs.tar.xz 
 if [ -f "$FILE" ]; then
     bash fedora_mod.sh
 else
     wget https://raw.githubusercontent.com/drunkwcodes/AnLinux-Resources/master/Scripts/Installer/Fedora/fedora.sh && bash fedora.sh
+    bash fedora_mod.sh
     cp "$FILE" "$BACKUP"
 fi
 
