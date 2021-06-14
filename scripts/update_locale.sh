9a231587f0f616b8cf09f3cef48160eec95fd292
Drunkwcodes <drunkwcodes@gmail.com>
2021-06-14 20:56:40 +0800

    Revert .bashrc, add user-dirs.dirs, setenv WIP

--- scripts/update_locale.sh
+++ scripts/update_locale.sh
@@ -9,9 +9,12 @@ mkdir ~/Templates/
 mkdir ~/Videos/
 
 # write config file
-echo "en_US" > ~/.config/user-dirs.locale 
+echo "en_US" > ~/.config/user-dirs.locale
+
+cp ./user-dirs.dirs ~/.config/
 
 # update
-xdg-user-dirs-update 
+xdg-user-dirs-update
+xdg-user-dirs-gtk-update
 
 echo "User dirs locale updated. Reboot to see the result."
