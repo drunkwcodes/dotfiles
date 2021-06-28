32f5d64f6784606595429478592a0437e3ba5a3f
Drunkwcodes <drunkwcodes@gmail.com>
2021-06-28 18:54:47 +0800

    Make updates later

--- ubuntu/setenv.sh
+++ ubuntu/setenv.sh
@@ -1,20 +1,3 @@
-sudo apt update
-sudo apt upgrade
-sudo apt install python3 python3-pip python3-venv vim git -y
-
-# This script assumes the machine uses bash and has git and pip3.
-
-# git config
-git config --global user.name "Drunkwcodes"
-git config --global user.email drunkwcodes@gmail.com
-git config --global core.editor vim
-git config --global merge.tool vimdiff
-
-# From http://garmoncheg.blogspot.com/2012/06/pretty-git-log.html
-git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
-
-# pip user install 
-pip3 install --user -U autopep8 ipython pytest pytest-cov black
 
 PROJ_ROOT=~/projects/dotfiles
 
@@ -40,3 +23,23 @@ ln -s "$PROJ_ROOT"/.bash_aliases ~/.bash_aliases
 # Add .vimrc
 mv ~/.vimrc ~/.vimrc.bak
 ln -s "$PROJ_ROOT"/.vimrc ~/.vimrc
+
+
+# Update, Installation
+sudo apt update
+sudo apt upgrade
+sudo apt install python3 python3-pip python3-venv vim git -y
+
+# This script assumes the machine uses bash and has git and pip3.
+
+# git config
+git config --global user.name "Drunkwcodes"
+git config --global user.email drunkwcodes@gmail.com
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+
+# From http://garmoncheg.blogspot.com/2012/06/pretty-git-log.html
+git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+
+# pip user install
+pip3 install --user -U autopep8 ipython pytest pytest-cov black
