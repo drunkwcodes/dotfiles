465dd1ab0583ad42f341859dc9b82cb18fb14292
Drunkwcodes <drunkwcodes@gmail.com>
2021-07-14 20:17:12 +0800

    Change layout order

--- scripts/qtile/config.py
+++ scripts/qtile/config.py
@@ -141,10 +141,10 @@ for i in groups:
 
 layouts = [
     # layout.Columns(border_focus_stack="#d75f5f"),
-    layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     layout.Bsp(),
+    layout.Max(),
     # layout.Matrix(),
     # layout.MonadTall(),
     # layout.MonadWide(),
