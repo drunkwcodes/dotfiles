13768a5141c22885ab6b667f8ef0e39991d4efa7
Drunkwcodes <drunkwcodes@gmail.com>
2023-04-21 09:56:42 +0800

    Add cmd aliases

---
+++ windows/cmd/bashrc.bat
@@ -0,0 +1,13 @@
+@echo off
+doskey ga=git add
+doskey gb=git branch
+doskey gc=git commit -m
+doskey gca=git commit --amend
+doskey gco=git checkout
+doskey gd=git diff
+doskey gde=git describe
+doskey glg=git lg
+doskey gp=git push
+doskey gpl=git pull --rebase
+doskey gs=git status
+doskey gsh=git stash
