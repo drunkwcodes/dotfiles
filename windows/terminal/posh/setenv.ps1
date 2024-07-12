a5b2c42937b81694af5e0c6c73fb8fd7433775de
Drunkwcodes <drunkwcodes@gmail.com>
2024-07-12 23:17:59 +0800

    Add my info

--- windows/terminal/posh/setenv.ps1
+++ windows/terminal/posh/setenv.ps1
@@ -1,3 +1,6 @@
 New-Item -ItemType SymbolicLink -Path $PROFILE -Target ".\\Microsoft.PowerShell_profile.ps1"
 
 git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
+
+git config --global user.name "Drunkwcodes"
+git config --global user.email drunkwcodes@gmail.com
