74df7549a771a102550dc4a6f5a58ac078990694
Drunkwcodes <drunkwcodes@gmail.com>
2024-07-24 14:51:22 +0800

    Update

--- ubuntu/setenv.sh
+++ ubuntu/setenv.sh
@@ -79,4 +79,4 @@ bind -f  ~/.inputrc
 # Typescript things
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install 20
-npm install -g typescript
+npm install -g typescript ts-node
