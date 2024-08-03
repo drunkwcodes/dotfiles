54622fa1f0f00e0442884793fc7fcdbcddd30d80
Drunkwcodes <drunkwcodes@gmail.com>
2024-08-03 18:33:44 +0800

    Update git main branch name setting.

--- ubuntu/setenv.sh
+++ ubuntu/setenv.sh
@@ -54,6 +54,7 @@ git config --global user.name "Drunkwcodes"
 git config --global user.email drunkwcodes@gmail.com
 git config --global core.editor vim
 git config --global merge.tool vimdiff
+git config --global init.defaultBranch main
 
 # From http://garmoncheg.blogspot.com/2012/06/pretty-git-log.html
 git config --global alias.lg "log --color --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
